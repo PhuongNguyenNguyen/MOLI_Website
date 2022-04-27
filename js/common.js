bea71274339210fa138b06ec1e924c09873a26fa
IsNguyen <ntphuongnguyen@gmail.com>
2022-04-27 09:08:15 +0700

    Version-1

---
+++ js/common.js
@@ -0,0 +1,83 @@
+/*!--------------------------------------------------------------------------------
+
+ Theme Name: Frontend Seed 4
+ Author:     trungnghia112 <trungnghia112@gmail.com>
+
+ -----------------------------------------------------------------------------------*/
+
+if (Modernizr.touch === true && window.innerWidth <= 767) {
+  //alert('Touch Screen');
+} else {
+
+}
+
+(function() {
+  'use strict'
+
+  /* ==================================================
+  # Get scroll bar width
+  ===================================================*/
+  function getBarwidth() {
+    // Create the measurement node
+    let scrollDiv = document.createElement('div')
+    scrollDiv.className = 'scrollbar-measure'
+    document.body.appendChild(scrollDiv)
+
+    // Get the scrollbar width
+    let scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth
+    //console.warn(scrollbarWidth); // Mac:  15
+
+    // Delete the DIV
+    document.body.removeChild(scrollDiv)
+    // console.log(scrollbarWidth);
+    return scrollbarWidth
+  }
+
+  function section1slick() {
+    $('.section1-js').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      //autoplay: true,
+      //autoplaySpeed: 7000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  }
+
+  function init() {
+    getBarwidth()
+    section1slick()
+  }
+
+  init()
+
+})()
