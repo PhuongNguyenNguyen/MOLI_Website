67559aa4481108ed2a41edc42cd99a7e723939c4
IsNguyen <ntphuongnguyen@gmail.com>
2022-05-04 13:40:45 +0700

    Version_3

--- js/common.js
+++ js/common.js
@@ -88,11 +88,34 @@ if (Modernizr.touch === true && window.innerWidth <= 767) {
       arrows: false,
     });
   }
+  function scroll_Top()
+  {
+    let mybutton = document.getElementById('myBtn');
+
+// When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = 'block';
+      } else {
+        mybutton.style.display = 'none';
+      }
+    }
+
+// When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+
+  }
 
   function init() {
     getBarwidth()
 //    section1slick()
     section1_2slick()
+    scroll_Top()
   }
 
   init()
